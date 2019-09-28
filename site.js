8e0e5ae6cdb996b8b70cc520ecf37a79837aca02
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-09-28 00:00:52 -0700

    Better animation usage and timers

--- site.js
+++ site.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
   number = document.getElementById("number");
   interval = setInterval(function() {
     iterateNumeral();
-  }, 100);
+  }, 2000);
 });
 
 // function modified from https://github.com/daneden/animate.css
-function animateCSS(element, animationName) {
-  const node = document.querySelector(element)
+function animateCSS(element, animationName, callback) {
+  const node = element;
   node.classList.add('animated', animationName)
 
   function handleAnimationEnd() {
@@ -23,23 +23,38 @@ function animateCSS(element, animationName) {
 }
 
 function iterateNumeral() {
-  number.innerText = Number(number.innerText) + 1;
-  if (Number(number.innerText) == 24) {
-    clearInterval(interval);
-    number.innerText = number.innerText + ".0";
-    interval = setInterval(function() {
-      addDecimal();
-    }, 100);
-  }
+  animateCSS(number, "fadeOutDown", function() {
+    number.innerText = Number(number.innerText) + 1;
+    animateCSS(number, "fadeInDown", function() {
+      if (Number(number.innerText) == 24) {
+        clearInterval(interval);
+        setTimeout(() => {
+          animateCSS(number, "fadeOutDown", function() {
+            number.innerText = number.innerText + ".1";
+            animateCSS(number, "fadeInDown", null);
+          });
+          interval = setInterval(function() {
+            addDecimal();
+          }, 2000);
+        }, 1000);
+      }
+    });
+  });
 }
 
 function addDecimal() {
   var endNumber = Number(number.innerText.slice(-1));
   if (endNumber == 9) {
     iterations += 1;
-    number.innerText = number.innerText + 0;
+    animateCSS(number, "fadeOutDown", function() {
+      number.innerText = number.innerText + 1;
+      animateCSS(number, "fadeInDown");
+    });
   }
   else {
-    number.innerText = "24." + "9".repeat(iterations) + (endNumber + 1);
+    animateCSS(number, "fadeOutDown", function() {
+      number.innerText = "24." + "9".repeat(iterations) + (endNumber + 1);
+      animateCSS(number, "fadeInDown");
+    });
   }
 }
