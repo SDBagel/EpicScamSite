bd2dd33d1dcf1fbf38f72ce89a6b7fd0c2206062
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-09-29 20:13:19 -0700

    Bettergofast

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 var number, percent, interval, iterations = 0;
-var gofasterspeed = 1000;
+var gofasterspeed = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
   number = document.getElementById("number");
@@ -10,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function iterateNumeral() {
+  if (Number(number.innerText) == 5)
+    document.getElementById("skipButton").style.opacity = 1;
+
   if (Number(number.innerText) == 24) {
     clearInterval(interval);
     document.getElementById("skipButton").style.opacity = 0;
@@ -17,34 +20,56 @@ function iterateNumeral() {
       document.getElementById("skipButton").style.display = "none";
     }, 1000);
     number.innerText = number.innerText + ".1";
+    document.getElementById("gofasterbutton").style.opacity = 1;
     recursive();
   }
   else 
     number.innerText = Number(number.innerText) + 1;
 
-  percent.innerText = (parseFloat(number.innerText) / 25).toString().substring(0, 4) + "% of the way there!";
+  percent.innerText = ((parseFloat(number.innerText) / 25) * 100).toString().substring(0, 4) + "% of the way there!";
 }
 
 function recursive() {
   setTimeout(function() {
-    percent.innerText = Math.min((parseFloat(number.innerText) / 25), 0.99).toString().substring(0, 4) + "% of the way there!"; 
+    percent.innerText = Math.min((parseFloat(number.innerText) / 25) * 100, 99.9).toString().substring(0, 4) + "% of the way there!"; 
     addDecimal();
-    recursive();
-  }, Math.min(Math.max((1000 - (200 * iterations)), 200), gofasterspeed));
+    if (iterations === 50) {
+      var faster = document.getElementById("gofasterbutton");
+      percent.innerText = "100% of the way there! Congrats!"
+      number.style.transitionDuration = "2s";
+      number.style.marginLeft = 0;
+      number.style.marginRight = 0;
+      number.style.transform = "";
+      number.style.width = "320px";
+      faster.style.opacity = 0;
+      setTimeout(() => {
+        faster.style.display = "none";
+        var claim = document.getElementById("claim");
+        claim.style.display = "unset";
+        claim.style.opacity = 1;
+        number.style.opacity = 0;
+        setTimeout(() => {
+          number.innerText = 25;
+          number.style.opacity = 1;
+        }, 1000);
+      }, 1000);
+    }
+    else recursive();
+  }, Math.max((1000 - (200 * iterations) - (100 * gofasterspeed)), 200 - (10 * gofasterspeed)));
 }
 
 function goFaster() {
-  number.style.transitionDuration = gofasterspeed + "ms";
-  gofasterspeed /= 2;
+  number.style.transitionDuration = (1000 - gofasterspeed * 100) + "ms";
+  gofasterspeed++;
   if (Number(number.innerText) <= 24) {
     clearInterval(interval);
-    interval = setInterval(function() { iterateNumeral() }, gofasterspeed);
+    interval = setInterval(function() { iterateNumeral() }, 1000 - (gofasterspeed * 100));
   }
 }
 
 function addDecimal() {
   var endNumber = Number(number.innerText.slice(-1));
-  if (endNumber == 9) {
+  if (endNumber === 9) {
     iterations += 1;
     number.innerText = number.innerText + 1;
     number.style.transform = "translateX(-" + (iterations * 72) + "px)";
